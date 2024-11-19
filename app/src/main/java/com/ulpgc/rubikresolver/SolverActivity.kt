f7630c5ec4437e9b1d53e6156c7794f9bbf07670
naixin9393 <naicinchen@gmail.com>
2024-11-19 01:23:02 +0000

    refactor: extract SolverRenderer and SolverScreen

--- app/src/main/java/com/ulpgc/rubikresolver/SolverActivity.kt
+++ app/src/main/java/com/ulpgc/rubikresolver/SolverActivity.kt
@@ -1,137 +1,17 @@
 package com.ulpgc.rubikresolver
 
-import android.content.Context
-import android.opengl.GLES20
-import android.opengl.GLSurfaceView
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import javax.microedition.khronos.egl.EGLConfig
-import javax.microedition.khronos.opengles.GL10
+import com.ulpgc.rubikresolver.ui.theme.screen.SolverScreen
 
 class SolverActivity : AppCompatActivity() {
-    private lateinit var glView: SolverGLSurfaceView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        
-        glView = SolverGLSurfaceView(this)
-        setContentView(glView)
-    }
-}
-
-class SolverGLSurfaceView(context: Context) : GLSurfaceView(context) {
-    init {
-        setEGLContextClientVersion(2)
-        setRenderer(SolverRenderer())
-    }
-}
-
-class SolverRenderer : GLSurfaceView.Renderer {
-    private lateinit var mSquare: Square
-    override fun onSurfaceCreated(unused: GL10, config: EGLConfig?) {
-        // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
-        mSquare = Square()
-    }
-
-    override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
-        // Adjust the viewport based on geometry changes,
-        // such as screen rotation
-        GLES20.glViewport(0, 0, width, height)
-    }
-
-    override fun onDrawFrame(unused: GL10) {
-        // Redraw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-        mSquare.draw()
-    }
-
-
-    class Square {
-        private var COORDS_PER_VERTEX = 3
-        private val squareCoords = floatArrayOf(
-            -0.5f,  0.5f, 0.0f,      // top left
-            -0.5f, -0.5f, 0.0f,      // bottom left
-            0.5f, -0.5f, 0.0f,       // bottom right
-            0.5f,  0.5f, 0.0f        // top right
-        )
-        private var color = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
-        private var positionHandle: Int = 0
-        private var mColorHandle: Int = 0
-
-        private val vertexCount: Int = squareCoords.size / COORDS_PER_VERTEX
-        private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
-
-        private var mProgram: Int
-        private val vertexShaderCode =
-            "attribute vec4 vPosition;" +
-                    "void main() {" +
-                    "  gl_Position = vPosition;" +
-                    "}"
-
-        private val fragmentShaderCode =
-            "precision mediump float;" +
-                    "uniform vec4 vColor;" +
-                    "void main() {" +
-                    "  gl_FragColor = vColor;" +
-                    "}"
-
-        init {
-            val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
-            val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
-
-            mProgram = GLES20.glCreateProgram().also {
-                GLES20.glAttachShader(it, vertexShader)
-                GLES20.glAttachShader(it, fragmentShader)
-                GLES20.glLinkProgram(it)
-            }
-        }
-
-
-        private val drawOrder = shortArrayOf(0, 1, 2, 0, 2, 3) // order to draw vertices
-        private val vertexBuffer = ByteBuffer.allocateDirect(squareCoords.size * 4).run {
-            order(ByteOrder.nativeOrder())
-            asFloatBuffer().apply {
-                put(squareCoords)
-                position(0)
-            }
-        }
-        private val drawListBuffer = ByteBuffer.allocateDirect(drawOrder.size * 2).run {
-            order(ByteOrder.nativeOrder())
-            asShortBuffer().apply {
-                put(drawOrder)
-                position(0)
-            }
-        }
-        private fun loadShader(type: Int, shaderCode: String): Int {
-            return GLES20.glCreateShader(type).also { shader ->
-                GLES20.glShaderSource(shader, shaderCode)
-                GLES20.glCompileShader(shader)
-            }
-        }
-
-        fun draw() {
-            GLES20.glUseProgram(mProgram)
-            val positionHandle: Int = GLES20.glGetAttribLocation(mProgram, "vPosition").also {
-                GLES20.glEnableVertexAttribArray(it)
-                GLES20.glVertexAttribPointer(
-                    it,
-                    COORDS_PER_VERTEX,
-                    GLES20.GL_FLOAT,
-                    false,
-                    vertexStride,
-                    vertexBuffer
-                )
-            }
-            mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
-                GLES20.glUniform4fv(colorHandle, 1, color, 0)
-            }
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, vertexCount)
-            GLES20.glDisableVertexAttribArray(positionHandle)
+        setContent {
+            SolverScreen()
         }
     }
-
 }
