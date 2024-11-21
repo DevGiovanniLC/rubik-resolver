d4e45b9546df2819e5bbca791743b91033fbb81e
naixin9393 <naicinchen@gmail.com>
2024-11-21 20:26:50 +0000

    add basic 3d cube

--- app/src/main/java/com/ulpgc/rubikresolver/opengl/objects/GLCube.kt
+++ app/src/main/java/com/ulpgc/rubikresolver/opengl/objects/GLCube.kt
@@ -4,13 +4,44 @@ import android.opengl.GLES20
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-class Square {
+class GLCube {
     private var coordinatesPerVertex = 3
     private val vertexCoordinates = floatArrayOf(
-        -0.5f, 0.5f, 0.0f,      // top left
-        -0.5f, -0.5f, 0.0f,      // bottom left
-        0.5f, -0.5f, 0.0f,       // bottom right
-        0.5f, 0.5f, 0.0f        // top right
+        // front
+        -0.5f, 0.5f, 0.5f,      // top left front
+        -0.5f, -0.5f, 0.5f,     // bottom left front
+        0.5f, -0.5f, 0.5f,      // bottom right front
+        0.5f, 0.5f, 0.5f,       // top right front
+
+        // back
+        -0.5f, 0.5f, -0.5f,     // top left back
+        -0.5f, -0.5f, -0.5f,    // bottom left back
+        0.5f, -0.5f, -0.5f,     // bottom right back
+        0.5f, 0.5f, -0.5f,       // top right back
+
+        // left
+        -0.5f, 0.5f, 0.5f,      // top left front
+        -0.5f, -0.5f, 0.5f,     // bottom left front
+        -0.5f, -0.5f, -0.5f,    // bottom left back
+        -0.5f, 0.5f, -0.5f,     // top left back
+
+        // right
+        0.5f, 0.5f, 0.5f,       // top right front
+        0.5f, -0.5f, 0.5f,      // bottom right front
+        0.5f, -0.5f, -0.5f,     // bottom right back
+        0.5f, 0.5f, -0.5f,      // top right back
+
+        // top
+        -0.5f, 0.5f, 0.5f,      // top left front
+        0.5f, 0.5f, 0.5f,       // top right front
+        0.5f, 0.5f, -0.5f,      // top right back
+        -0.5f, 0.5f, -0.5f,     // top left back
+
+        // bottom
+        -0.5f, -0.5f, 0.5f,     // bottom left front
+        0.5f, -0.5f, 0.5f,      // bottom right front
+        0.5f, -0.5f, -0.5f,     // bottom right back
+        -0.5f, -0.5f, -0.5f     // bottom left back
     )
     private var color = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
     private var colorBlack = floatArrayOf(0.0f, 0.0f, 0.0f, 1.0f)
@@ -83,6 +114,7 @@ class Square {
                 vertexBuffer
             )
         }
+/*
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
             GLES20.glUniform4fv(colorHandle, 1, color, 0)
         }
@@ -92,6 +124,7 @@ class Square {
         }
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, vertexCount)
+*/
 
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
             GLES20.glUniform4fv(colorHandle, 1, colorBlack, 0)
@@ -101,7 +134,12 @@ class Square {
         }
 
         GLES20.glLineWidth(10.0f)
-        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 0, vertexCount)
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 0, 4)
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 4, 4)
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 8, 4)
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 12, 4)
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 16, 4)
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 20, 4)
         GLES20.glDisableVertexAttribArray(mPositionHandle)
     }
 }
--- app/src/main/java/com/ulpgc/rubikresolver/opengl/renderer/SolverRenderer.kt
+++ app/src/main/java/com/ulpgc/rubikresolver/opengl/renderer/SolverRenderer.kt
@@ -3,19 +3,24 @@ package com.ulpgc.rubikresolver.opengl.renderer
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import com.ulpgc.rubikresolver.opengl.objects.GLCube
 import com.ulpgc.rubikresolver.opengl.objects.Square
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class SolverRenderer : GLSurfaceView.Renderer {
     private lateinit var mSquare: Square
+    private lateinit var mCube: GLCube
     private val viewMatrix = FloatArray(16)
     private val vPMatrix = FloatArray(16)
     private val projectionMatrix = FloatArray(16)
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig?) {
         // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        GLES20.glClearColor(0.157f, 0.627f, 1.0f, 1.0f)
+/*
         mSquare = Square()
+*/
+        mCube = GLCube()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
@@ -24,7 +29,7 @@ class SolverRenderer : GLSurfaceView.Renderer {
         GLES20.glViewport(0, 0, width, height)
         val aspectRatio: Float = width.toFloat() / height.toFloat()
 
-        Matrix.frustumM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, 3f, 7f)
+        Matrix.frustumM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, 0.5f, 10.0f)
     }
 
     override fun onDrawFrame(unused: GL10) {
@@ -32,11 +37,14 @@ class SolverRenderer : GLSurfaceView.Renderer {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
         // Set the camera position (View matrix)
-        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, 7f, 0f, 0f, 0f, 0f, 1f, 0f)
+        Matrix.setLookAtM(viewMatrix, 0, 3.0f, 2.4f, 3.0f, 0f, 0f, 0f, 0f, 1f, 0f)
 
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
+/*
         mSquare.draw(vPMatrix)
+*/
+        mCube.draw(vPMatrix)
     }
 }
