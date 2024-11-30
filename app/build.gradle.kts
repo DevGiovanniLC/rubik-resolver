5644ca14f65729c08f85456bb95ed8238e35b294
naixin9393 <naicinchen@gmail.com>
2024-11-30 18:43:27 +0000

    fix image orientation on camera activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,6 @@ android {
 }
 
 dependencies {
-    implementation (project(":opencv3"))
     implementation (libs.androidx.camera.camera2)
     implementation (libs.androidx.camera.lifecycle)
     implementation (libs.androidx.camera.view)
@@ -73,6 +72,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.lifecycle.common.jvm)
+    implementation(project(":opencv"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/ulpgc/rubikresolver/CameraActivity.kt
+++ app/src/main/java/com/ulpgc/rubikresolver/CameraActivity.kt
@@ -3,6 +3,11 @@ package com.ulpgc.rubikresolver
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.graphics.YuvImage
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -13,6 +18,7 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -27,16 +33,25 @@ import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.ulpgc.rubikresolver.components.CameraPreview
 import com.ulpgc.rubikresolver.ui.theme.RubikResolverTheme
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import java.io.ByteArrayOutputStream
 
 @ExperimentalMaterial3Api
 class CameraActivity : ComponentActivity() {
@@ -49,66 +64,80 @@ class CameraActivity : ComponentActivity() {
         }
 
         setContent {
-            RubikResolverTheme {
-                val scaffoldState = rememberBottomSheetScaffoldState()
-                val controller = remember{
-                    LifecycleCameraController(applicationContext).apply{
-                        setEnabledUseCases(
-                            CameraController.IMAGE_CAPTURE
-                        )
-                    }
-                }
-
-
-                BottomSheetScaffold(
-                    scaffoldState = scaffoldState,
-                    sheetPeekHeight = 0.dp,
-                    sheetContent = {}
-                ){
-                    padding ->
-                    Box(modifier = Modifier
-                        .fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ){
-
-                        CameraPreview(controller = controller,
-                            modifier = Modifier
-                                .fillMaxSize()
-                        )
-
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.BottomCenter)
-                                .padding(bottom = 100.dp)
-                                .height(100.dp)
-                                .width(100.dp),
-                            horizontalArrangement = Arrangement.SpaceAround
-                        ) {
-                            Button(
-                                onClick = {
-                                    takePhoto(
-                                        controller = controller,
-                                        onPhotoTaken = { bitmap ->
-                                            onTakePhoto(bitmap)
-                                        },
-                                    )
-                                },
-                                modifier = Modifier.size(50.dp),
-                                shape = CircleShape,
-                                colors = ButtonDefaults.buttonColors(containerColor = Color.White)
-                            ) {
-
-                            }
-                        }
-                    }
-                }
-
-
-            }
+            CameraScreen()
         }
     }
 
+    @Composable
+    private fun CameraScreen() {
+        var mockImage by remember { mutableStateOf<Bitmap?>(null) }
+        RubikResolverTheme {
+            val scaffoldState = rememberBottomSheetScaffoldState()
+            val controller = remember {
+                LifecycleCameraController(applicationContext).apply {
+                    setEnabledUseCases(
+                        CameraController.IMAGE_CAPTURE
+                    )
+                }
+            }
+
+
+            BottomSheetScaffold(
+                scaffoldState = scaffoldState,
+                sheetPeekHeight = 0.dp,
+                sheetContent = {}
+            ) { padding ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+
+                    CameraPreview(
+                        controller = controller,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 100.dp)
+                            .height(100.dp)
+                            .width(100.dp),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Button(
+                            onClick = {
+                                takePhoto(
+                                    controller = controller,
+                                    onPhotoTaken = { bitmap ->
+                                        onTakePhoto(bitmap)
+                                        mockImage = bitmap
+                                    },
+                                )
+                            },
+                            modifier = Modifier.size(50.dp),
+                            shape = CircleShape,
+                            colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+                        ) {
+
+                        }
+                    }
+                    if (mockImage != null) {
+                        Image(
+                            bitmap = mockImage!!.asImageBitmap(),
+                            contentDescription = "Mock image",
+                            modifier = Modifier.size(400.dp)
+                        )
+                    }
+                }
+            }
+
+
+        }
+    }
 
 
     private fun hasRequiredPermissions(): Boolean {
@@ -134,7 +163,14 @@ class CameraActivity : ComponentActivity() {
           object : OnImageCapturedCallback(){
               override fun onCaptureSuccess(image: ImageProxy) {
                   super.onCaptureSuccess(image)
-                  onPhotoTaken(image.toBitmap())
+
+                  val rotationDegrees = image.imageInfo.rotationDegrees
+                  val bitmap = image.toBitmap()
+                  val matrix = Matrix()
+                  matrix.postRotate(rotationDegrees.toFloat())
+                  val correctedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+
+                  onPhotoTaken(correctedBitmap)
               }
 
               override fun onError(exception: ImageCaptureException) {
@@ -156,6 +192,51 @@ class CameraActivity : ComponentActivity() {
         Log.d("BitmapProperties", "Width: $width, Height: $height")
         Log.d("BitmapProperties", "Config: $config, ByteCount: $byteCount")
         Log.d("BitmapProperties", "Density: $density")
+
+        // Process image with OpenCV
+        processImage(bitmap)
     }
 
+    private fun processImage(bitmap: Bitmap) {
+        // Process image with OpenCV
+        val mat = Mat()
+        Utils.bitmapToMat(bitmap, mat)
+
+        // Detect tiles
+        detectTiles(mat)
+
+        Utils.matToBitmap(mat, bitmap)
+
+        // Display processed image
+
+    }
+
+    private fun detectTiles(mat: Mat) {
+        // Detect tiles
+
+    }
+
+    fun ImageProxy.toBitmap(): Bitmap {
+        val yBuffer = planes[0].buffer
+        val uBuffer = planes[1].buffer
+        val vBuffer = planes[2].buffer
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        // Copy Y, U, and V buffers into NV21 array
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+
+        // Convert NV21 format to Bitmap
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, width, height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(Rect(0, 0, width, height), 100, out)
+        val imageBytes = out.toByteArray()
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    }
 }
